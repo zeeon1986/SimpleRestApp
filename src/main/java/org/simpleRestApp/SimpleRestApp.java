9f30e37a1c9bffb7fd6f52db670e71e7df1cc4ad
evgeniy <zeeon1986@gmail.com>
2020-08-28 19:25:34 +0300

    Add first Controller

---
+++ src/main/java/org/simpleRestApp/SimpleRestApp.java
@@ -0,0 +1,12 @@
+package org.simpleRestApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleRestApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleRestApp.class, args);
+    }
+}
---
+++ src/main/java/org/simpleRestApp/controller/MessageController.java
@@ -0,0 +1,59 @@
+package org.simpleRestApp.controller;
+
+import org.simpleRestApp.exceptions.NotFoundException;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("message")
+public class MessageController {
+    private int counter = 4;
+    private List<Map<String, String>> messages = new ArrayList<>() {{
+        add(new HashMap<String, String>() {{ put("id", "1"); put("text", "First Message"); }});
+        add(new HashMap<String, String>() {{ put("id", "2"); put("text", "Second Message"); }});
+        add(new HashMap<String, String>() {{ put("id", "3"); put("text", "Third Message"); }});
+    }};
+
+    @GetMapping
+    public List<Map<String, String>> list() {
+        return messages;
+    }
+
+    @GetMapping("{id}")
+    public Map<String, String> getOne(@PathVariable String id) {
+        return getMessage(id);
+    }
+
+    private Map<String, String> getMessage(@PathVariable String id) {
+        return messages.stream().filter(message -> message.get("id").equals(id)).findFirst().orElseThrow(NotFoundException::new);
+    }
+
+    @PostMapping
+    public Map<String, String> create(@RequestBody Map<String, String> message) {
+        message.put("id", String.valueOf(counter++));
+        messages.add(message);
+
+        return message;
+    }
+
+    @PutMapping("{id}")
+    public Map<String, String> update(@PathVariable String id, @RequestBody Map<String, String> message) {
+        Map<String, String> messageFromDB = getMessage(id);
+
+        messageFromDB.putAll(message);
+        messageFromDB.put("id", id);
+
+        return messageFromDB;
+    }
+
+    @DeleteMapping("{id}")
+    public void delete(@PathVariable String id) {
+        Map<String, String> messageFromDB = getMessage(id);
+
+        messages.remove(messageFromDB);
+    }
+}
